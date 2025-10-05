8b05dd86f4582470279f951fb73fbdaec0cac9bb
abhimanyutr <abhimanyurajan396@gmail.com>
2025-10-05 19:01:58 +0530

    //

---
+++ src/components/Singlecard.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import "./singleCard.css"
+import { Box, Stack, Button } from '@mui/material';
+function Singlecard({card,handleChoice,flipped,disabled}) {
+
+    const handleClick = ()=>{
+        if(!disabled){
+             handleChoice(card)
+        }
+       
+    }
+
+
+  return (
+    
+       <div
+    className={`card ${flipped ? "flipped" : ""}`}
+    onClick={handleClick}  // Optional: move click to whole card
+    key={card.id}
+  >
+    <img className="front" src={card.src} alt="card front" />
+    <img className="back" src="/images/coverImg.jpg" alt="card back" />
+  </div>
+    
+  )
+}
+import "./singlecard.css"
+export default Singlecard
